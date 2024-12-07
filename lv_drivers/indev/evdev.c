bcec138ad237244143ba92c6d5610cb23022ba7d
Paul <paul.hollander001@gmail.com>
2024-12-07 14:26:41 +0100

    touch update

--- lv_drivers/indev/evdev.c
+++ lv_drivers/indev/evdev.c
@@ -55,6 +55,7 @@ int evdev_key_val;
 void evdev_init(void)
 {
 	char str[2048];
+	char name[256] = "Unknown";
 	int input_device = 0;
 	char *ptr;
 	
@@ -65,8 +66,7 @@ void evdev_init(void)
 		sprintf(str, "/dev/input/event%d", input_device);
 		evdev_fd = open(str, O_RDONLY | O_NONBLOCK);
 		if (evdev_fd > 0)
-		{
-			char name[256] = "Unknown";
+		{	
 			ioctl(evdev_fd, EVIOCGNAME(sizeof(name)), name);
 			printf("Input device name: \"%s\"\n", name);
 			ptr = strstr(name, "raspberrypi-ts");
@@ -87,8 +87,8 @@ void evdev_init(void)
 		}
 		else
 			input_device++;
-	} while (input_device < 30 && evdev_fd == -1) ;	
-
+	} while (input_device < 30 && evdev_fd == -1) ;
+	printf("Touch device name: \"%s\"\n", name);
     if(evdev_fd == -1) {
         perror("unable open evdev interface:");
         return;
